b02a8dff74687438ad30fc491721cc1a82ac5790
bookeezhou <bookeezhou@gmail.com>
2020-12-21 14:33:55 +0800

    修改 cargo run 不能作为input的情况

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -20,6 +20,23 @@
   ;;(define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
   )
 
+(with-eval-after-load 'rust-mode
+  (define-key rust-mode-map (kbd "C-e C-e C-r") 'my-cargo-run))
+(defun my-cargo-run ()
+  "Build and run Rust code."
+  (interactive)
+  (cargo-process-run)
+  (let (
+      (orig-win (selected-window))
+      (run-win (display-buffer (get-buffer "*Cargo Run*") nil 'visible))
+    )
+    (select-window run-win)
+    (comint-mode)
+    (read-only-mode 0)
+    (select-window orig-win)
+  )
+)  
+
 (use-package flycheck-rust
   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
